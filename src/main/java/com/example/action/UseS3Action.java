3c0019ba8535ec760e9284d342283dc3e31859f8
佐藤大翔 <105324471+SatoHiroto526@users.noreply.github.com>
2025-12-22 08:26:09 +0900

    バゲット、プレフィックス削除

--- src/main/java/com/example/action/UseS3Action.java
+++ src/main/java/com/example/action/UseS3Action.java
@@ -26,9 +26,9 @@ import lombok.Setter;
 @Setter
 public class UseS3Action {
 
-    private String bucket = "uses3byjakartaee";
+    private String bucket = "";
 
-    private String prefix = "com/example/";
+    private String prefix = "";
 
     private String file;
 
